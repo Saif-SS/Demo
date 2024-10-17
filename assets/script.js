b2ee34d6bc78e307d2ff5a6b29f8c58375f2e094
Kristoffer <kristoffer.benckert@chasacademy.se>
2024-10-17 10:58:43 +0200

    string to int on input

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const participants = (document.getElementById("participants"));
-  const total = (document.getElementById("total"));
-  const tip = (document.getElementById("tip"));
-
   let submitButton = document.getElementById('buttonSubmit');
-
+  
   // Hämta alla input-fält som är required
   const requiredInputs = document.querySelectorAll('input[required]');
   const form = document.getElementById('myForm');
-
+  
   // Lägg till keyup-lyssnare för alla required-fält
   requiredInputs.forEach(function(input) {
     input.addEventListener('keyup', function() {
       validateForm();
     });
   });
-
+  
   function validateForm() {
     requiredInputs.forEach(function(input) {
-
+      
       // Kontrollera om hela formuläret är giltigt
       if (form.checkValidity()) {
         submitButton.disabled = false;
@@ -31,13 +27,17 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     });
   };
-
+  
   submitButton.addEventListener('click', calculate);
   
   function calculate(e) {
     e.preventDefault();
-    let val = parseInt(participants);
-    console.log(typeof val, val);
-  }
 
+    let participants = Number(document.getElementById("participants").value);
+    let total = Number(document.getElementById("total").value);
+    let tip = Number(document.getElementById("tip").value);
+
+    console.log(typeof participants, participants, typeof total, total, typeof tip, tip);
+  }
+  
 });
